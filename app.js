5fca7d5a620eeed5a1ae61556d8d028e61b56ee0
Mark Scott <mscott@papagodev.com>
2022-02-03 21:00:44 -0700

    Added env var

--- app.js
+++ app.js
@@ -55,9 +55,11 @@ watcher.on('add', file_path => {
     var file_name = path.basename(file_path).split('.')[0].replace("_original", "");
     console.log(`File ${uploaded_file} has been added`);
 
-    getYouTubePromo(uploaded_file, file_name, "4K").run();
+    //getYouTubePromo(uploaded_file, file_name, "4K").run();
     getYouTubePromo(uploaded_file, file_name, SCREEN_WIDTH_720, "Twitter-720").run();
-    getYouTubePromo(uploaded_file, file_name, SCREEN_WIDTH_1080, "1080").run();
+    getYouTubePromo(uploaded_file, file_name, SCREEN_WIDTH_1080, "LinkedIn-1080").run();
+
+    getYouTube(uploaded_file, file_name).run();
 });
 
 
@@ -69,16 +71,20 @@ function getYouTubePromo(uploaded_file, file_name, _screen_width, title){
         inputs: '1', outputs: 'ckout'
     });
     filterChain.push({
-        filter: 'overlay', options: null,
-        inputs: ['0', 'ckout'], outputs: 'overlay_out'
+        filter: 'scale', options: '3840:-1',
+        inputs: 'ckout', outputs: 'overlay_scaled'
     });
     filterChain.push({
         filter: 'scale', options: '3840:-1',
-        inputs: 'overlay_out', outputs: 'scaled'
+        inputs: '0', outputs: 'main_scaled'
+    });
+    filterChain.push({
+        filter: 'overlay', options: "(main_w-overlay_w)/2:(main_h-overlay_h)/2:format=auto",
+        inputs: ['main_scaled', 'overlay_scaled'], outputs: 'overlay_out'
     });
     filterChain.push({
         filter: 'concat', options: "n=2:v=1:a=1",
-        inputs: ['scaled', '0:a','2', '2:a'], outputs: ['youtube_promo_vid', 'youtube_promo_audio']
+        inputs: ['overlay_out', '0:a','2', '2:a'], outputs: ['youtube_promo_vid', 'youtube_promo_audio']
     });
     if(_screen_width && _screen_width != SCREEN_WIDTH_4k){
         filterChain.push({
@@ -116,6 +122,69 @@ function getYouTubePromo(uploaded_file, file_name, _screen_width, title){
         .addOutputOption("-preset ultrafast")
         .output(directory + file_name + "_youtube_promo_"+ _title + ".mp4")
         .map(screen_width && screen_width != SCREEN_WIDTH_4k ? "youtube_promo_vid_scaled" : "youtube_promo_vid")
+        .map("youtube_promo_audio")
+
+        .on('start', (cmdline) => console.log(cmdline))
+        .on('error', function (err, stdout, stderr) {
+            console.log('Error: ' + err.message);
+            console.log('ffmpeg output:\n' + stdout);
+            console.log('ffmpeg stderr:\n' + stderr);
+
+        })
+        .on('progress', function (data) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write(JSON.stringify(data));
+            //process.stdout.write("Complete: " + Math.round(data.percent) + '%');
+        })
+        .on('end', function(stdout, stderr) {
+            console.log('Transcoding succeeded !');
+            console.log(stdout);
+          })
+}
+
+function getYouTube(uploaded_file, file_name){
+    
+    var filterChain = []
+    
+    filterChain.push({
+        filter: 'chromakey', options: '0x00ff00:0.25:0.0',
+        inputs: '1', outputs: 'ckout'
+    });
+    filterChain.push({
+        filter: 'scale', options: '3840:-1',
+        inputs: 'ckout', outputs: 'overlay_scaled'
+    });
+    filterChain.push({
+        filter: 'scale', options: '3840:-1',
+        inputs: '0', outputs: 'main_scaled'
+    });
+    filterChain.push({
+        filter: 'overlay', options: "(main_w-overlay_w)/2:(main_h-overlay_h)/2:format=auto",
+        inputs: ['main_scaled', 'overlay_scaled'], outputs: 'overlay_out'
+    });
+    filterChain.push({
+        filter: 'concat', options: "n=2:v=1:a=1",
+        inputs: ['overlay_out', '0:a','2', '2:a'], outputs: ['youtube_promo_vid', 'youtube_promo_audio']
+    });
+
+
+    var directory = path.resolve("processed/") + "\\" + file_name + "\\";
+    fs.mkdir('processed/' + file_name + "/", { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+    console.log(`Directory ${directory} set`);
+
+    return ffmpeg()
+        .input(uploaded_file)
+        .input(path.resolve("assets/intro_overlay.mp4"))
+        .input(path.resolve("assets/end_youtube_more.mp4"))
+        .addOptions(['-vsync '])
+        .complexFilter(filterChain)
+        .toFormat('mp4')
+        .addOutputOption("-preset ultrafast")
+        .output(directory + file_name + "_YouTube-3840.mp4")
+        .map("youtube_promo_vid")
         .map("youtube_promo_audio")
 
         .on('start', (cmdline) => console.log(cmdline))
